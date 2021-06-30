13d0bd2875500af6793ddc0f3ae02efc43d24149
Murat Şimşek <udemydeveloper99@gmail.com>
2021-06-30 23:15:59 +0300

    Devam edilecek

--- Abc.MvcWebUI/Controllers/HomeController.cs
+++ Abc.MvcWebUI/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Abc.MvcWebUI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller //For publish
     {
         DataContext _context = new DataContext(); //DataInitializer (veritabanını) tanıtma
         
